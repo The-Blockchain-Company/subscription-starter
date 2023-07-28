e2e52a16f58de3100c78c8492e245ea723f942fa
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-07-28 12:11:22 -0400

    lg

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images : {
+    domains : ['quantumone.b-cdn.net', 'unpkg.com', 'lh3.googleusercontent.com'],
+  },
 };
 
 module.exports = nextConfig;
