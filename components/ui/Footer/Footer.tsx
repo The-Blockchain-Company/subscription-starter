7bf732b27532a4e64d882e440ca8d92088788dde
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-08-05 17:25:20 -0400

    .

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -3,8 +3,50 @@ import Link from 'next/link';
 import Logo from '@/components/icons/Logo';
 import GitHub from '@/components/icons/GitHub';
 import s from './Footer.module.css';
+import { useState } from 'react';
+
+
 
 export default function Footer() {
+  const [userInput, setUserInput] = useState("");
+
+  const handleSubmit = async (event: { preventDefault: () => void; target: { Name: { value: any; }; Email: { value: any; }; }; }) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault();
+ 
+    // Get data from the form.
+    const data = {
+      first: event.target.Name.value,
+      last: event.target.Email.value,
+    };
+ 
+    // Send the data to the server in JSON format.
+    const JSONdata = JSON.stringify(data);
+ 
+    // API endpoint where we send form data.
+    const endpoint = '/api/form';
+ 
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: 'POST',
+      // Tell the server we're sending JSON.
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    };
+ 
+    // Send the form data to our forms API on Vercel and get a response.
+    const response = await fetch(endpoint, options);
+ 
+    // Get the response data from server as JSON.
+    // If server returns the name submitted, that means the form works.
+    const result = await response.json();
+    alert(`Is this your full name: ${result.data}`);
+  };
+
   return (
     <>
     <div className="container my-24 mx-auto md:px-6">
@@ -19,7 +61,7 @@ export default function Footer() {
         
       </div>
       <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:mb-0 md:w-6/12 md:px-3 lg:px-6">
-        <form action="/api/form" method="post">
+        <form action="/api/form" method="post" onSubmit={handleSubmit}>
           <div className="relative mb-6" data-te-input-wrapper-init>
             <input type="text"
               className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
@@ -41,7 +83,7 @@ export default function Footer() {
           <div className="relative mb-6" data-te-input-wrapper-init>
             <textarea
               className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-              id="exampleFormControlTextarea1" rows={3} placeholder="Your message"></textarea>
+              id="exampleFormControlTextarea1" rows={3} value={userInput} placeholder="Your message" onChange={e => setUserInput(e.target.value)}></textarea>
             <label htmlFor="exampleFormControlTextarea1"
               className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Message</label>
           </div>
@@ -53,7 +95,7 @@ export default function Footer() {
               Send me a copy of this message
             </label>
           </div>
-          <button type="button" data-te-ripple-init data-te-ripple-color="light"
+          <button type="submit" data-te-ripple-init data-te-ripple-color="light"
             className="mb-6 inline-block w-full rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
             Send
           </button>
