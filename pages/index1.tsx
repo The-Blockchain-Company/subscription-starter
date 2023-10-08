ef6418ad654abd91826883916835a1a083f215f4
Robert Mourey Jr <rmourey_jr@quantumone.io>
2023-10-07 22:18:28 -0400

    Update index1.tsx

--- pages/index1.tsx
+++ pages/index1.tsx
@@ -87,8 +87,8 @@ export default function Home() {
         <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-          <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blog</h2>
-          <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">We employ technical SEO strategies to produce SEO scores above the average of the top 100 sites.</p>
+          <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">BlockSquawk</h2>
+          <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400"> Trends and analysis on business use cases across web 3.0, AI, and quantum computing. </p>
       </div> 
       <div className="grid gap-8 lg:grid-cols-2">
           <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
