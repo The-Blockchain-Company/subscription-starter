df3448624ef56246b5e37033ef4f6019a2bcdafd
Robert Mourey Jr <rmourey_jr@quantumone.io>
2023-10-07 22:04:41 -0400

    Update index1.tsx

--- pages/index1.tsx
+++ pages/index1.tsx
@@ -88,7 +88,7 @@ export default function Home() {
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
           <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blog</h2>
-          <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
+          <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">We employ technical SEO strategies to produce SEO scores above the average of the top 100 sites.</p>
       </div> 
       <div className="grid gap-8 lg:grid-cols-2">
           <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
@@ -100,12 +100,12 @@ export default function Home() {
                   <span className="text-sm">14 days ago</span>
               </div>
               <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">How to quickly deploy a static website</a></h2>
-              <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
+              <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.</p>
               <div className="flex justify-between items-center">
                   <div className="flex items-center space-x-4">
                       <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
                       <span className="font-medium dark:text-white">
-                          Jese Leos
+                          Robert Mourey Jr
                       </span>
                   </div>
                   <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
@@ -128,7 +128,7 @@ export default function Home() {
                   <div className="flex items-center space-x-4">
                       <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" />
                       <span className="font-medium dark:text-white">
-                          Bonnie Green
+                          Robert Mourey Jr
                       </span>
                   </div>
                   <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
