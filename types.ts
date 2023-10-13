717eca4bdcdf5326bb6708802c9e96490f2a334c
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-10-13 18:37:36 -0400

    Update types.ts

--- types.ts
+++ types.ts
@@ -73,19 +73,3 @@ export interface Subscription {
   trial_end?: string;
   prices?: Price;
 }
-
-type Sitemap = Array<{
-  url: string
-  lastModified?: string | Date
-  changeFrequency?:
-    | 'always'
-    | 'hourly'
-    | 'daily'
-    | 'weekly'
-    | 'monthly'
-    | 'yearly'
-    | 'never'
-  priority?: number
-}>
-
-export interface Sitemap[]
