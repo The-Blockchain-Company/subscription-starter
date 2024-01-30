041a878ce4ce851e064a818a80bca692ff338417
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-01-30 15:41:52 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => [
+  {
+    source: "/public/resume.html",
+    destination: "/app/api/resume.js",
+  },
+],
   images : {
     domains : ['quantumone.b-cdn.net', 'unpkg.com', 'lh3.googleusercontent.com'],
   },
