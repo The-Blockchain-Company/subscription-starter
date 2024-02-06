38a30134cf22733cad8261dd5b02ab34ffe9ded0
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-02-05 23:57:24 -0500

    Update Pricing.tsx

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -65,7 +65,7 @@ export default function Pricing({ products }: Props) {
            <div className="sm:flex sm:flex-col sm:align-center">
              <>
 <Link href="/signin" className={cn('mt-10 align-center self-center bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow',
-                              {s.link},)}>
+                              s.link)}>
                 Sign up to subscribe!
               </Link>
               </>
